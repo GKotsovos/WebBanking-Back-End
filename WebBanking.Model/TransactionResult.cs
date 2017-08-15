7bf20255e64bbf10ac97d8595f047643b12acebb
Gerasimos Kotsovos <kotsovos.gerasimos@gmail.com>
2017-08-15 22:33:41 +0300

    Added Transaction Result class

---
+++ WebBanking.Model/TransactionResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBanking.Model
+{
+    public class TransactionResult
+    {
+        public bool HasError { get; set; }
+        public string Message { get; set; }
+        public TransactionResult(bool hasError, string message)
+        {
+            this.HasError = hasError;
+            this.Message = message;
+        }
+    }
+}
