63fa07e346cde680a3047e7386638066ff2075c5
Gerasimos Kotsovos <kotsovos.gerasimos@gmail.com>
2017-08-20 22:45:09 +0300

    Renamed the loan transaction model

--- WebBanking.Model/LoanTransaction.cs
+++ WebBanking.Model/LoanTransaction.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 
 namespace WebBanking.Model
 {
-    public class LoanPaymentDetails
+    public class LoanTransaction
     {
         public string LoanId { get; set; }
         public string DebitAccount { get; set; }
+        public string DebitAccountType { get; set; }
         public decimal Amount { get; set; }
         public string Currency { get; set; }
         public DateTime Date { get; set; }
